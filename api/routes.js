9b28c73b1f45f3924a809097c911eacbdacf7396
Ekaterina <nataveres@gmail.com>
2020-05-28 17:33:49 +0200

    Fronend finished

--- api/routes.js
+++ api/routes.js
@@ -1,11 +1,17 @@
-const controllers = require('./controllers.js');
-const express = require('express');
+const controllers = require("./controllers.js");
+const express = require("express");
 
 const router = express.Router();
 
-router.get('/', controllers.hello);
+router.get("/", controllers.hello);
 
 // write your routes
+router.get("/books", controllers.getList);
 
+router.get("/books/:id", controllers.getBook);
+
+router.post("/books", controllers.addBook);
+
+router.delete("/books/:id", controllers.removeBook);
 
 module.exports = router;
--- client/app/init.js
+++ client/app/init.js
@@ -1,9 +1,46 @@
-fetch('/api')
-  .then(res => res.json())
-  .then(data => {
-    console.log(data)
-    document.getElementById('root')
-      .innerHTML = data.message;
-  })
-  .catch(err => console.error(err));
+//import renderBookList from "./view.js";
 
+// fetch("/api")
+//   .then((res) => res.json())
+//   .then((data) => {
+//     console.log(data);
+//     document.getElementById("root").innerHTML = data.message;
+//   })
+//   .catch((err) => console.error(err));
+
+fetch("/api/books")
+  .then((res) => {
+    if (!res.ok) {
+      throw res;
+    }
+    return res.json();
+  })
+  .then((books) => {
+    renderBookList(books); //write
+  })
+  .catch((err) => console.error(err));
+
+document.getElementById("save-button").addEventListener("click", (e) => {
+  let book = {};
+  const author = e.target.form.author.value;
+  book.author = author;
+  const title = e.target.form.title.value;
+  book.title = title;
+  const available = e.target.form.avail.value;
+  book.available = Boolean(available);
+  if (e.target.form.year.value) {
+    const year = e.target.form.year.value;
+    book.year = Number(year);
+  }
+  if (e.target.form.page.value) {
+    const pages = e.target.form.page.value;
+    book.pages = Number(pages);
+  }
+  if (e.target.form.kind.value) {
+    const kind = e.target.form.kind.value;
+    book.kind = { genre: kind }; //let it be like this
+  }
+  console.log(book);
+  saveBook(book); //write
+  e.preventDefault();
+});
